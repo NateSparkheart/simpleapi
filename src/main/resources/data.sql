b64ea6bd2a2d435a849fc95a34678e4efe98a1b5
Nate <kappavlad25@gmail.com>
2022-02-25 01:13:52 +0300

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.product (name, brand, quantity, price) values ('Радиоприемник', 'Звезда', '12', '143400');
+INSERT INTO public.product (name, brand, quantity, price) values ('Телеграфный аппарат', 'Стрела', '13', '54500');
+INSERT INTO public.product (name, brand, quantity, price) values ('Коммутатор', 'Звезда', '4', '18000');
+INSERT INTO public.product (name, brand, quantity, price) values ('Радиопередатчик', 'Звезда', '12', '158900');
+INSERT INTO public.product (name, brand, quantity, price) values ('Рабочее место администратора', 'Стрела', '1', '200000');
